4aad4b5f22423e441791550c8e7fb5bda25ab30b
yxj <564197835@qq.com>
2021-09-07 21:04:33 +0800

    add MusicController

---
+++ MusicFish/ScriptingBridge/MusicController.swift
@@ -0,0 +1,29 @@
+// MusicController.swift
+
+import Cocoa
+import ScriptingBridge
+
+class MusicController {
+    static let shared = MusicController()
+    
+    let MusicApplication: AnyObject
+    
+    init() {
+        MusicApplication = SBApplication(bundleIdentifier: "com.apple.Music")!
+    }
+    
+    func playPause() {
+        // toggle the playing/paused state of the current track
+        MusicApplication.playpause()
+    }
+    
+    func nextTrack() {
+        // advance to the next track in the current playlist
+        MusicApplication.nextTrack()
+    }
+    
+    func previousTrack() {
+        // return to the previous track in the current playlist
+        MusicApplication.previousTrack()
+    }
+}
